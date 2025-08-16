8ee04da4d85f8a0dbbc7e68745be22b468d68fb0
Kassidy Satterfield <kassidy.satterfield@outlook.com>
2025-08-16 12:47:59 -0500

    Update concat.py

--- concat.py
+++ concat.py
@@ -1,6 +1,6 @@
-from pypdf import PdfMerger
+from pypdf import PdfMerger #import that had the needed file merger
 
-files_to_merge = ["invoice123_page1.pdf", "invoice123_page2.pdf"]
+files_to_merge = ["invoice123_page1.pdf", "invoice123_page2.pdf"] 
 merger = PdfMerger()
 for pdf in files_to_merge:
     merger.append(pdf)
